50ad593f0c4bca8f44e26e0c36c9dd2646864615
Mateusz Kuchta <mtkuchta@gmail.com>
2023-01-24 20:57:46 +0100

    fonts config

--- src/assets/styles/GlobalStyles.js
+++ src/assets/styles/GlobalStyles.js
@@ -2,6 +2,9 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
 
+/* font-family: 'Roboto', sans-serif;
+font-family: 'Roboto Slab', serif; */
+
 *{
     margin:0;
     padding:0;
@@ -10,6 +13,7 @@ export const GlobalStyle = createGlobalStyle`
 html{
     color:#151619;
     font-size:10px;
+
 }
 *,*::after, *::before{
     box-sizing:inherit;
@@ -17,12 +21,12 @@ html{
     padding:0;
 }
 body{
-    font-family: 'Montserrat', sans-serif;
+    font-family: 'Roboto', sans-serif;
     font-size:10px;
     box-sizing:border-box
 }
 a,button,input{
-    font-family:'Montserrat', sans-serif;
+    font-family:'Roboto', sans-serif;
 }
 input[type=number]::-webkit-inner-spin-button, 
 input[type=number]::-webkit-outer-spin-button { 
--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -5,7 +5,16 @@ export const theme = {
     button: '#E46643',
     hover: '#F39765',
   },
-  fontSize: {},
+  fontSize: {
+    heading_M: '15px',
+    heading_S: '14px',
+    appBody: '13px',
+    h1: '32px',
+    h2: '28px',
+    h3: '24px',
+    h4: '20px',
+    h5: '16px',
+    h6: '14px',
+    text: '14px',
+  },
 };
-
-/* Rectangle */
